ef23bfc0df955c15eff5cd6a3eec0ead7b4ccded
sam <verhasseltsamuelj@gmail.com>
2013-02-26 02:00:19 -0800

    Made some adjustments to the default data

--- server/server.js
+++ server/server.js
@@ -3,12 +3,18 @@ Meteor.startup(function () {
     if (Books.find().count() === 0) {
       var books = [ { title: "Applied Combinatorics",
                    author: "Alan Tucker",
+                   professor: "Dr. Mostag",
+                   studentclass: "Discrete Methods",
+                   school: "lmu",
                    isbn: 9780470458389 },
                    { title: "NOT APPLIED Combinatorics",
                    author: "Alan Tucker",
-                   isbn: 8675309 }];
+                   professor: "Dr. Mostag",
+                   studentclass: "Discrete Methods",
+                   school: "lmu",
+                   isbn: 1234567980 }];
       for (var i = 0; i < books.length; i++)
-        Books.insert({title: books[i].title, author: books[i].author, edition: books[i].edition, isbn: books[i].isbn});
+        Books.insert({title: books[i].title, author: books[i].author, edition: books[i].edition, isbn: books[i].isbn, professor: books[i].professor, studentclass: books[i].studentclass, school: books[i].school, date: new Date()});
     }
   });
 
@@ -34,7 +40,7 @@ Books.allow({
 
     remove: function (userId, books) {
         return !_.any(books, function (book) {
-            // deny if not the owner, or if other people are going
+            // deny if not the owner
             return book.owner !== userId;
         });
     }
