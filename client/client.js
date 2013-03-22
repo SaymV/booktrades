4e7fb95bd5642a2fcc2ef84504f3b8fb71fca500
sam <verhasseltsamuelj@gmail.com>
2013-03-22 16:22:03 -0700

    Another stab at google analytics.

--- client/client.js
+++ client/client.js
@@ -239,6 +239,18 @@ var toggleCreateDialog = function (value) {
     Session.set("showCreateDialog", value);
 };
 
+Template.page.created = function() {
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-37728411-1']);
+          _gaq.push(['_setDomainName', 'booktrad.es']);
+          _gaq.push(['_trackPageview']);
+
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+};
 
 Template.page.showCreateDialog = function () {
     return Session.get("showCreateDialog");
